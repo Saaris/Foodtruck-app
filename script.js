345adc83723a997593cf075263a610a965ee5c72
Sara Serti <saaris1985@gmail.com>
2024-12-09 08:55:23 +0100

    gör om koden

--- script.js
+++ script.js
@@ -1,30 +1,149 @@
-// GET = hämtar någonting, POST = skickar med någonting
+// GET = hämtar någonting, POST = vi skickar med någonting
 // POST (då behöver vi skicka med en body)
-// API-Key: yum-3PqATVLPR8zw2xRn
 
-const baseUrl = 'https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com';
 
-let apiKey;
+// const url = 'https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com';
 
-const getKey = async () => {
-  try {
-    const response = await fetch(`${baseUrl}/keys`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+// let apiKey;
 
-    if (!response.ok) return;
+// const getKey = async () => {
+//   try {
+//     const response = await fetch(`https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/keys`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
 
-    const keyData = await response.json();
-    apiKey = keyData.key;
-    console.log(apiKey);
-    return keyData.key;
-  } catch (error) {
-    console.log('det gick inte att..', error);
-  }
-};
+//     if (!response.ok) return;
 
-getKey();
+//     const keyData = await response.json();
+//     apiKey = keyData.key;
+//     console.log(apiKey);
+//     return keyData.key;
+//   } catch (error) {
+//     console.log('det gick inte att..', error);
+//   }
+// };
+
+// getKey();
+
+// const getTenant = async () => {
+//     const key = await getKey(); 
+    
+//     try {
+//       const bodyToParse = {
+//         'name': 'Sara S',
+//       };
+  
+//       const response = await fetch(`https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/tenants`, {
+//         method: 'POST',
+//         name: "x-zocom",
+//         body: JSON.stringify(bodyToParse),
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'x-zocom': key,
+//         },
+//       });
+  
+//       if (!response.ok) return;
+  
+//       const tenantData = await response.json();
+  
+//       localStorage.setItem('user', JSON.stringify(tenantData));
+  
+//       return tenantData.id;
+//     } catch (error) {
+//       console.log('error:', error);
+//     }
+//   };
+  
+//   getTenant();
+
+
+
+// const url = 'https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/';
+//   let apiKey= 'yum-toeJ8M4AzH5F1cFK'
+
+
+// const bodyToSend = {
+//     name: 'Ann di Feynd'
+// }
+// const options = {
+//     method: 'POST',
+//     body: JSON.stringify(bodyToSend)
+// }
+// fetch(url, options)
+
+
+//     try {
+//     const response = await fetch(url,options)
+//         console.log('status är: ', response.status);
+
+//         const data = await response.json()
+//         console.log('data från API: ', data);
+
+
+// } catch (error) {
+//     console.log('det gick inte ...', error.message);
+
+// }
+
+
+// const getTenant = {
+//     name: 'Sara S'
+// }
+// const options = {
+//     method: 'POST',
+//     headers: {
+//     "Content-Type": 'application/json',  // vi skickar JSON i body
+//     "x-zocom": apiKey  // din API-nyckel
+// },
+// body: JSON.stringify(bodyToSend)
+// }
+// fetch(url, options)
+
+//'yum-PxtRFopRoKZwir25'
+
+const url = 'https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/keys'
+    const bodyToSend = {
+    name: 'x-zocom'
+}
+    const options = {
+    method: 'POST',
+    headers: {
+        "Content-Type": 'application/json',  // vi skickar JSON i body
+
+    },
+
+    body: JSON.stringify(bodyToSend)
+}
+
+    try {
+    const response = await fetch(url,options)
+        console.log('status är: ', response.status);
+
+        const data = await response.json()
+        console.log('data från API: ', data);
+
+
+} catch (error) {
+    console.log('det gick inte ...', error.message);
+
+}
+
+const getTenantUrl = 'https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/tenants'
+
+const option = {
+        method: 'POST',
+        headers: {
+        "Content-Type": 'application/json', 
+        "x-zocom": 'yum-PxtRFopRoKZwir25'
+    },
+    body: JSON.stringify(bodyToSend)
+}
+    fetch(url, option)
+
+
+
 
