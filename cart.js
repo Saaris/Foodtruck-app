0380aee2871d74a59a554f58292ffe4879b85135
Sara Serti <saaris1985@gmail.com>
2024-12-13 11:03:42 +0100

    visa valda i ordervy

--- cart.js
+++ cart.js
@@ -1,4 +1,21 @@
-const cartElement = document.querySelector("#cart")
+const cartItem = document.querySelector("#cart")
 const cartItemsElement = document.querySelector("#cart-items")
 export const cart = []
 
+export const showCart = () => {
+    cartItem.classList.remove("hidden")
+
+    cartItemsElement.textContent = "";
+    
+    const chosenItems = []
+    cart.forEach((item) => {
+        if (chosenItems.includes(item.name)) 
+        
+        const itemElement = document.createElement("div")
+        const quantity = cart.filter(item => item.name === item.name).length;
+        const price = (item.price * quantity)
+        
+        itemElement.classList.add('item')
+    })
+    
+    }
